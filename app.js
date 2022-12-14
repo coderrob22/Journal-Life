3d8ff3a59a51adea9ba4261d572621c00108e9eb
coderrob22 <robertknott7@gmail.com>
2022-12-14 14:43:37 -0500

    Added MongoStore back to the app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars')
 const passport= require('passport')
 const methodOverride= require('method-override')
 const session= require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 
